89aa3b0872954629bcc6df7f831cf2885862ef00
mohammad nasiri <nasiri.mohammadreza92@yahoo.com>
2023-06-09 00:13:10 +0330

    create header animation

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// var scrollBar = document.querySelector(".scroll-bar")
+// window.addEventListener("scroll", function (e) {
+//    var scrolly = scrollY
+//    var height = document.documentElement.scrollHeight - window.innerHeight
+//    var heightBar = (scrolly / height) * 100
+//    scrollBar.style.width = `${heightBar}%`
+// })
+
+
+
+
+
+var header = document.querySelector(".header")
+window.addEventListener("scroll" , function(e){
+    var x = 1 - (scrollY / window.innerHeight)
+    header.style.opacity = x
+
+})
